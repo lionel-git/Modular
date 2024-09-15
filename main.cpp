f6216f285269e40c48e5708b8f42ee4367d8f73b
Lionel Desorme <lionel.desorme@free.fr>
2024-09-15 14:36:06 +0200

    Update tests

--- main.cpp
+++ main.cpp
@@ -17,13 +17,57 @@ uint64_t modular_mult(uint64_t a, uint64_t b, uint64_t n)
 }
 #endif
 
+uint64_t get_inverse(int64_t b, int64_t modulo)
+{
+    int64_t r0 = modulo;
+    int64_t r1 = b;
+    int64_t s0 = 1;
+    int64_t s1 = 0;
+    int64_t t0 = 0;
+    int64_t t1 = 1;
+
+    while (r1 != 0)
+    {
+        int64_t q = r0 / r1;
+        int64_t r2 = r0 - q * r1;
+        int64_t s2 = s0 - q * s1;
+        int64_t t2 = t0 - q * t1;
+
+        r0 = r1;
+        r1 = r2;
+        s0 = s1;
+        s1 = s2;
+        t0 = t1;
+        t1 = t2;
+    }
+    if (r0 == 1)
+    {
+        if (t0 < 0)
+            t0 += modulo;
+        return t0;
+    }
+    else
+        throw std::runtime_error("gcd is not one");
+}
+
+void check_inverse(int64_t a, int64_t modulo)
+{
+    int64_t inverse = get_inverse(a, modulo);
+    int64_t result = modular_mult(a, inverse, modulo);
+    if (result != 1)
+        throw std::runtime_error("inverse is not correct");
+}
+
 int main(int argc, char* argv[]) 
 {
-    uint64_t modulo = 18446744073709551557ull;
-    uint64_t a = 12345678901234567890ull;
-    uint64_t b = 15765432109876543210ull;
-    uint64_t result = modular_mult(a, b, modulo);
+    int64_t m = 9223372036854775783ll;
+    int64_t a = 7345678901234567890ll % m;
+    int64_t b = 5765432109876543210ll % m;
+    int64_t result = modular_mult(a, b, m);
     std::cout << "Result: " << result << std::endl;
+
+    check_inverse(a, m);
+    check_inverse(b, m);
     return 0;
 }
 
